db2fd40bb1c6399240a1d3fade0c0d58371ccdbc
Lordkaito <cespedesisai88@gmail.com>
2022-02-07 13:29:58 +0100

    Fix linter errors

--- test/system/entities_test.rb
+++ test/system/entities_test.rb
@@ -1,39 +1,39 @@
-require "application_system_test_case"
+require 'application_system_test_case'
 
 class EntitiesTest < ApplicationSystemTestCase
   setup do
     @entity = entities(:one)
   end
 
-  test "visiting the index" do
+  test 'visiting the index' do
     visit entities_url
-    assert_selector "h1", text: "Entities"
+    assert_selector 'h1', text: 'Entities'
   end
 
-  test "should create entity" do
+  test 'should create entity' do
     visit entities_url
-    click_on "New entity"
+    click_on 'New entity'
 
-    click_on "Create Entity"
+    click_on 'Create Entity'
 
-    assert_text "Entity was successfully created"
-    click_on "Back"
+    assert_text 'Entity was successfully created'
+    click_on 'Back'
   end
 
-  test "should update Entity" do
+  test 'should update Entity' do
     visit entity_url(@entity)
-    click_on "Edit this entity", match: :first
+    click_on 'Edit this entity', match: :first
 
-    click_on "Update Entity"
+    click_on 'Update Entity'
 
-    assert_text "Entity was successfully updated"
-    click_on "Back"
+    assert_text 'Entity was successfully updated'
+    click_on 'Back'
   end
 
-  test "should destroy Entity" do
+  test 'should destroy Entity' do
     visit entity_url(@entity)
-    click_on "Destroy this entity", match: :first
+    click_on 'Destroy this entity', match: :first
 
-    assert_text "Entity was successfully destroyed"
+    assert_text 'Entity was successfully destroyed'
   end
 end
